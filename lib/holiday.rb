6610a3add06e8516aa435f928f96ffa7aa15483f
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-19 22:28:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,9 +20,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   
   holiday_hash.each do |k,v1| 
-    k.each do |k2,v2|
-      return k2[1] if k2 == :fourth_of_july
-    end 
+    return v1[1] if k == :fourth_of_july
   end 
   
 end
