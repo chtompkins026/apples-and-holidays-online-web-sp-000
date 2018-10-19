9212dba644c4d767cdc680dafe044aad55739216
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-19 22:38:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,6 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_hash[:winter][:Christmas][2] = supply
+  holiday_hash[:winter][:new_years][1] = supply
 end
 
 
